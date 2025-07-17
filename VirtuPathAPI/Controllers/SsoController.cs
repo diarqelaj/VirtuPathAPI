47ff3ecb73c6f344c5f10a0c2365d32e00ad99b4
Taulant74 <taulantr2004@gmail.com>
2025-07-17 15:55:08 +0200

    ,

--- VirtuPathAPI/Controllers/SsoController.cs
+++ VirtuPathAPI/Controllers/SsoController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.IO;
-using Microsoft.AspNetCore.Http;
 
 namespace VirtuPathAPI.Controllers
 {
@@ -16,13 +16,12 @@ namespace VirtuPathAPI.Controllers
             "login-status.html"
         );
 
+        // 1) Serves HTML and sets the VirtuPathSession cookie
         [HttpGet("login-status")]
         public IActionResult LoginStatus()
         {
-            // 1) Generate or retrieve the JWT for the current user
             var jwt = GenerateOrRetrieveJwtForCurrentUser();
 
-            // 2) Set it as a cookie (no Domain so it defaults to the host serving the HTML)
             Response.Cookies.Append("VirtuPathSession", jwt, new CookieOptions
             {
                 HttpOnly = true,
@@ -31,11 +30,18 @@ namespace VirtuPathAPI.Controllers
                 Expires = DateTimeOffset.UtcNow.AddDays(7)
             });
 
-            // 3) Serve the static HTML page which contains your postMessage script
             if (!System.IO.File.Exists(_htmlPath))
                 return NotFound("SSO HTML page not found.");
 
             return PhysicalFile(_htmlPath, "text/html");
+        }
+
+        // 2) Returns the JWT as JSON for postMessage retrieval
+        [HttpGet("token")]
+        public IActionResult GetToken()
+        {
+            var jwt = GenerateOrRetrieveJwtForCurrentUser();
+            return Ok(new { token = jwt });
         }
 
         private string GenerateOrRetrieveJwtForCurrentUser()
