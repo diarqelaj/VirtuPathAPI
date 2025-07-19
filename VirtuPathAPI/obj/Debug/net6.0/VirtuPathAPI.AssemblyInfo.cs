f9ca3cea58b3c1a9f345207866c15b85d8d39842
Dijar <diarqelaj15@gmail.com>
2025-07-20 00:58:51 +0200

    Program cs updatev2

--- VirtuPathAPI/obj/Debug/net6.0/VirtuPathAPI.AssemblyInfo.cs
+++ VirtuPathAPI/obj/Debug/net6.0/VirtuPathAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtuPathAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe7dee504df43c9f958dfaf8bde47acf610a4de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465f6e0cd84494eee11fceea3e22245a2da7eb75")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtuPathAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtuPathAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
