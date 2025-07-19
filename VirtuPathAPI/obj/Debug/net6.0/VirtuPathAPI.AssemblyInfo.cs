6a628b3f7848eaefdd856cc98db53903cdac7e9f
Dijar <diarqelaj15@gmail.com>
2025-07-19 16:46:45 +0200

    Dockerfile update

--- VirtuPathAPI/obj/Debug/net6.0/VirtuPathAPI.AssemblyInfo.cs
+++ VirtuPathAPI/obj/Debug/net6.0/VirtuPathAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtuPathAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6db4d3e2eaa98faa1c2a7ca610e114f41fa7d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41711b43f52310fd51c3889c61696d0dc769ec51")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtuPathAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtuPathAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
